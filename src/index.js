81976a08bafca0f613828494f8ac9109c76e400f
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-30 13:31:31 +0300

    add DOMContentLoaded eventListener

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
-function fetchActivity (){
+document.addEventListener("DOMContentLoaded", () => {
     apiContainer.classList.replace('invisible','visible')
-    header.classList.replace('visible','invisible')
+    // header.classList.replace('visible','invisible')
     getActivityButton.style.top = '75%'
     getActivityButton.style.transform = 'translate (-50%,-50%) scale(1.3)'
 
+    getActivityButton.addEventListener('click', () => {
     fetch ('http://www.boredapi.com/api/activity/')
     .then (res => res.json())
     .then (data => {
-        console.log(data)
-        if (data.link){
-            activity.innerHTML = `<span class = "poppins">Activity: </span><a href = ${data.link}><span class = "bold animation">${data.activity}</span></a>`
-            type.innerHTML = `<span class = "poppins">Type: </span><span class = "montserrat">${data.type}</span>`
-            participants.innerHTML = `<span class = "poppins">Participants: </span><span class = "montserrat">${data.participants}</span>`
-            accessibility.innerHTML = `<span class = "poppins">Accessibility: </span><span class = "montserrat">${data.accessibility}</span>`
-        } else {
-            activity.innerHTML = `<span class = "poppins">Activity: </span><span class = "montserrat">${data.activity}</span>`
-            type.innerHTML = `<span class = "poppins">Type: </span><span class = "montserrat">${data.type}</span>`
-            participants.innerHTML = `<span class = "poppins">Participants: </span><span class = "montserrat">${data.participants}</span>`
-            accessibility.innerHTML = `<span class = "poppins">Accessibility: </span><span class = "montserrat">${data.accessibility}</span>`
+    console.log(data)
+    if (data.link){
+        activity.innerHTML = `<span class = "poppins">Activity: </span><a href = ${data.link}><span class = "bold animation">${data.activity}</span></a>`
+        type.innerHTML = `<span class = "poppins">Type: </span><span class = "montserrat">${data.type}</span>`
+        participants.innerHTML = `<span class = "poppins">Participants: </span><span class = "montserrat">${data.participants}</span>`
+        accessibility.innerHTML = `<span class = "poppins">Accessibility: </span><span class = "montserrat">${data.accessibility}</span>`
+    } else {
+        activity.innerHTML = `<span class = "poppins">Activity: </span><span class = "montserrat">${data.activity}</span>`
+        type.innerHTML = `<span class = "poppins">Type: </span><span class = "montserrat">${data.type}</span>`
+        participants.innerHTML = `<span class = "poppins">Participants: </span><span class = "montserrat">${data.participants}</span>`
+        accessibility.innerHTML = `<span class = "poppins">Accessibility: </span><span class = "montserrat">${data.accessibility}</span>`
         }
     })
-}
-getActivityButton.addEventListener('click', fetchActivity)
+})
+})
+
 
 function submitComment(){
     let data = "";  
