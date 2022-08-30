8744d2f3a8fd59520b7cf8ede448c1165bd212df
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-30 12:52:29 +0300

    add comment form functionality

--- src/index.js
+++ src/index.js
@@ -22,3 +22,13 @@ function fetchActivity (){
     })
 }
 getActivityButton.addEventListener('click', fetchActivity)
+
+function submitComment(){
+    let data = "";  
+    let name = document.getElementById("userName").value
+    let email = document.getElementById("userEmail").value
+    let comment = document.getElementById("userComment").value
+
+    data = "User name : "+name+"<br/>User email : "+email+ "<br/>User comment : "+comment
+    document.getElementById("data").innerHTML = data  // display data to paragraph
+}
